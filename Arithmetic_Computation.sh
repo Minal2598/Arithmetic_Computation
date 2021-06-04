5da0bde01098feab9b9ea2008fe1b216e5eb9535
Minal <patilminalp@gmail.com>
2021-06-04 19:10:16 +0530

    Compute expression

--- Arithmetic_Computation.sh
+++ Arithmetic_Computation.sh
@@ -3,3 +3,6 @@ echo "Computes Different Arithmetic Expression and sorts the results"
 read -p "Enter First number:" a
 read -p "Enter Second number:" b
 read -p "Enter Third number:" c
+
+Expression1=$(($a+$(($b*$c))))
+echo "Result of Expression1 is::"$Expression1
