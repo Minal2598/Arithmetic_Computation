839f01b33513d426571b9d07cf5eb2a0a9f3a7e8
Minal <patilminalp@gmail.com>
2021-06-04 21:38:12 +0530

    Added:stroe result in dictionary

--- Arithmetic_Computation.sh
+++ Arithmetic_Computation.sh
@@ -1,18 +1,34 @@
 echo "Computes Different Arithmetic Expression and sorts the results"
 
+declare -A Arithmetic
+
 read -p "Enter First number:" a
 read -p "Enter Second number:" b
 read -p "Enter Third number:" c
 
+
 Expression1=$(($a+$(($b*$c))))
 echo "Result of Expression1 {a+b*c} is::"$Expression1
+
+
 
 Expression2=$(($(($a*$b))+$c))
 echo "Result of Expression2  {a*b+c} is::"$Expression2
 
+
+
+
 Expression3=$(($c+$(($a/$b))))
 echo "Result of Expression3  {c+a/b} is::"$Expression3
+
+
+
 
 Expression4=$(($(($a%$b))+$c))
 echo "Result of Expression4  {a%b+c} is::"$Expression4
 
+
+Arithmetic+=( ["1"]=$Expression1 ["2"]=$Expression2 ["3"]=$Expression3 ["4"]=$Expression4 )
+
+echo ${Arithmetic[@]}
+echo ${Arithmetic[@]}
