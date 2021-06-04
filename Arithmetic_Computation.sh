7d8f16f1eb599c110e52c574d2b707259bff7206
Minal <patilminalp@gmail.com>
2021-06-04 18:56:06 +0530

    Added:take three input

--- Arithmetic_Computation.sh
+++ Arithmetic_Computation.sh
@@ -1,1 +1,5 @@
-Computes Different Arithmetic Expression and sorts the results
+echo "Computes Different Arithmetic Expression and sorts the results"
+
+read -p "Enter First number:" a
+read -p "Enter Second number:" b
+read -p "Enter Third number:" c
