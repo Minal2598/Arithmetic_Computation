f1564b346c5e98d091181338bfbeccf8934c9c94
Minal <patilminalp@gmail.com>
2021-06-05 00:18:42 +0530

    sort and computing result in des_cOrder

--- Arithmetic_Computation.sh
+++ Arithmetic_Computation.sh
@@ -6,23 +6,14 @@ read -p "Enter First number:" a
 read -p "Enter Second number:" b
 read -p "Enter Third number:" c
 
-
 Expression1=$(($a+$(($b*$c))))
 echo "Result of Expression1 {a+b*c} is::"$Expression1
-
-
 
 Expression2=$(($(($a*$b))+$c))
 echo "Result of Expression2  {a*b+c} is::"$Expression2
 
-
-
-
 Expression3=$(($c+$(($a/$b))))
 echo "Result of Expression3  {c+a/b} is::"$Expression3
-
-
-
 
 Expression4=$(($(($a%$b))+$c))
 echo "Result of Expression4  {a%b+c} is::"$Expression4
@@ -38,3 +29,5 @@ done
 echo "Array:"${arr[@]}
 
 echo "Dictionary" ${Arithmetic[@]}
+
+printf '%s\n' "${arr[@]}" | sort -nr
