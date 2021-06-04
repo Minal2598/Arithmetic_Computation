956b843c2f397fee6cf7e0dcb03dc4ebd61c39fe
Minal <patilminalp@gmail.com>
2021-06-04 23:47:30 +0530

    Dictonary into Array

--- Arithmetic_Computation.sh
+++ Arithmetic_Computation.sh
@@ -1,7 +1,7 @@
 echo "Computes Different Arithmetic Expression and sorts the results"
 
 declare -A Arithmetic
-
+arr=()
 read -p "Enter First number:" a
 read -p "Enter Second number:" b
 read -p "Enter Third number:" c
@@ -27,8 +27,14 @@ echo "Result of Expression3  {c+a/b} is::"$Expression3
 Expression4=$(($(($a%$b))+$c))
 echo "Result of Expression4  {a%b+c} is::"$Expression4
 
-
 Arithmetic+=( ["1"]=$Expression1 ["2"]=$Expression2 ["3"]=$Expression3 ["4"]=$Expression4 )
 
-echo ${Arithmetic[@]}
-echo ${Arithmetic[@]}
+
+for i in ${Arithmetic[@]}
+do 
+	arr+=($i)
+done 
+
+echo "Array:"${arr[@]}
+
+echo "Dictionary" ${Arithmetic[@]}
